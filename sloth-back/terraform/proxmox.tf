0c42c820984d5435cf95edefb1077be50862e5b3
Guigui0812 <guillaume.rohee0812@gmail.com>
2024-03-22 16:48:22 +0100

    proxmox OK

--- sloth-back/terraform/proxmox.tf
+++ sloth-back/terraform/proxmox.tf
@@ -1,27 +1,20 @@
-resource "proxmox_vm_qemu" "test-vm" {
-    name = "terraform-vm"
-    desc = "VM created by Terraform"
-    target_node = "infra"
-    clone = "ubuntu-22-10-template"
-    full_clone = "true"
-    agent = 1
-    os_type = "cloud-init"
-    cores = 2
-    sockets = 1
-    cpu = "host"
-    memory = 2048
-    
-    disk {
-        type = "scsi"
-        size = "10G"
-        storage = "sloth-vms"
+resource "proxmox_virtual_environment_vm" "debian_vm" {
+  name      = "test-debian"
+  node_name = "infra"
+
+  initialization {
+    user_account {
+      username = "user"
+      password = "password"
     }
+  }
 
-    network {
-        model = "virtio"
-        bridge = "vmbr0"
-        tag = "18"
-    }
-
-
+  disk {
+    datastore_id = "local-lvm"
+    file_id      = "local:iso/ubuntu-22.10-server-cloudimg-amd64.img"
+    interface    = "virtio0"
+    iothread     = true
+    discard      = "on"
+    size         = 20
+  }
 }
