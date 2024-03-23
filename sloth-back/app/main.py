5cfe9e99ed9548bdf62e91b0f93e7ab0f1a711fe
Theo Lomege <theolomege@gmail.com>
2024-03-23 18:43:29 +0100

    Rest fonctionnel (GET/POST/DELETE) sur AWS)     Prochain travaux :     - Méthode PUT     - Révisions des appels API pour utiliser des paramètres (--target terraform pour supprimer une instance cible)

--- sloth-back/app/main.py
+++
@@ -1,7 +0,0 @@
-from fastapi import FastAPI
-from deployments.routers import router as deployment_router
-
-app = FastAPI()
-
-# Inclusion des routers de chaque sous-module
-app.include_router(deployment_router, prefix="/deployment")
