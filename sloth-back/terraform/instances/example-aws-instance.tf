b581b760cb676a4c7475497501510d3a3ac009b4
Theo Lomege <theolomege@gmail.com>
2024-03-24 14:24:30 +0100

    Mise à jour de l'API et des méthodes pour créer une instance en fonction du provider BUG : Impossible de créer une instance proxmox car la gestion de module est erronée

--- sloth-back/terraform/instances/example-aws-instance.tf
+++
@@ -1,8 +0,0 @@
-# Paramètres de la configuration
-resource "aws_instance" "example-aws-instance" {
-  ami = "ami-00c71bd4d220aa22a"
-  instance_type = "t2.micro"
-  tags = {
-    Name = "example-aws-instance"
-  }
-}
