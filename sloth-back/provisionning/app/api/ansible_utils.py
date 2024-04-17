144dccf1df69a02b3369bbfb073b5b74e5366056
Guigui0812 <guillaume.rohee0812@gmail.com>
2024-04-17 10:20:45 +0200

    change hierarchy playbook

--- sloth-back/provisionning/app/api/ansible_utils.py
+++ sloth-back/provisionning/app/api/ansible_utils.py
@@ -3,7 +3,7 @@ import pathlib
 
 def run_ansible_playbook(host : str, playbook_service : str):
     try:
-        output = subprocess.run(["ansible-playbook", "-i", "../ansible/inventory", f"../ansible/playbooks/{playbook_service}.yml"])
+        output = subprocess.run(["ansible-playbook", "-i", "../ansible/inventory", f"../ansible/playbooks/code/{playbook_service}.yml"])
         return {"success": True, "message": output.stdout}
     except subprocess.CalledProcessError as e:
         return {"success": False, "message": str(e)}
