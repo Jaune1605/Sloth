11c8d4d341ae08694370ff4fc770b2d8d1292ace
Guigui0812 <guillaume.rohee0812@gmail.com>
2024-04-17 15:29:16 +0200

    Add print

--- sloth-back/provisionning/app/api/ansible_utils.py
+++ sloth-back/provisionning/app/api/ansible_utils.py
@@ -4,8 +4,12 @@ import pathlib
 def run_ansible_playbook(host : str, playbook_service : str):
     try:
         output = subprocess.run(["ansible-playbook", "-i", "../ansible/inventory", f"../ansible/playbooks/code/{playbook_service}.yml", "-l", host], capture_output=True)
+        print(output.stdout)
+        print(output.stderr)
         return {"success": True, "message": output.stdout}
     except subprocess.CalledProcessError as e:
+        return {"error": False, "message": str(e)}
+    except Exception as e:
         return {"error": False, "message": str(e)}
 
 def get_ansible_playbooks():
