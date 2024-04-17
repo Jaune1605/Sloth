4399af8510aed08565019f453a704a44b82346fc
Guigui0812 <guillaume.rohee0812@gmail.com>
2024-04-17 09:18:50 +0200

    cors policies

--- sloth-back/provisionning/app/main.py
+++ sloth-back/provisionning/app/main.py
@@ -10,15 +10,9 @@ import uvicorn
 
 app = FastAPI()
 
-origins = [
-    "http://localhost:3000",  # Adapté pour ton frontend Angular, React, Vue, etc.
-    "http://localhost:4200",
-    "http://127.0.0.1:4200"
-]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,  # Autoriser les cookies cross-origin
     allow_methods=["*"],  # Autoriser toutes les méthodes
     allow_headers=["*"],  # Autoriser tous les headers
